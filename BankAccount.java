04989776ae68884916057532fedaf3e07f174864
jzhong10 <junzhong2003@gmail.com>
2020-10-27 21:30:42 -0400

    Implemented and tested code for depositation and withdrawation

--- BankAccount.java
+++ BankAccount.java
@@ -25,8 +25,25 @@ public class BankAccount {
       return password;
     }
     */
-    
+
     public void setPassword(String newPass) {
       password = newPass;
     }
+
+    public boolean deposit(double amount) {
+      if (amount >= 0) {
+        balance += amount;
+        return true;
+      }
+      return false;
+    }
+
+    public boolean withdraw(double amount) {
+      if (amount < 0 || amount > balance) {
+        return false;
+      }
+      balance-=amount;
+      return true;
+    }
+    
 }
--- BankTester.java
+++ BankTester.java
@@ -1,10 +1,30 @@
 public class BankTester {
     public static void main(String[] args) {
       BankAccount a1 = new BankAccount(1234, "easy");
-      System.out.println(a1.getPassword());
+      int x = 0;
+      int y = 0;
+      if (args.length == 1) {
+        x = Integer.parseInt(args[0]);
+      } else if (args.length == 2) {
+        x = Integer.parseInt(args[0]);
+        y = Integer.parseInt(args[1]);
+      }
+
+    //  System.out.println(a1.getPassword());
       a1.setPassword("strong");
-      System.out.println(a1.getPassword());
+
+    //  System.out.println(a1.getPassword());
       System.out.println(a1.getBalance());
       System.out.println(a1.getAccountID());
+
+      System.out.println();
+
+      a1.deposit(100);
+      System.out.println(a1.getBalance());
+      a1.withdraw(50);
+      System.out.println(a1.getBalance());
+      a1.withdraw(x);
+      System.out.println(a1.getBalance());
+      System.out.println(x);
     }
 }
